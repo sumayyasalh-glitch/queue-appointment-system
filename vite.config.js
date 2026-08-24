7d14c116a6ea1509ad6655fb65c6a0516b1c9652
sumayya <sumayyasalh@gmail.com>
2026-08-25 02:10:10 +0530

    finished

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+/* global process */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/queue-appointment-system/',
+  base: process.env.GITHUB_ACTIONS ? '/queue-appointment-system/' : '/',
   plugins: [react()],
 })
